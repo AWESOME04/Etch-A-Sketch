c7da8ad3eb0b53e0d2965db57d05de8d3349be6b
Evans Acheampong <evansachie01@gmail.com>
2024-09-01 19:53:02 +0000

    feat: add event listener for bg color change

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const mainDiv = document.getElementById('container');
 
-for(i = 0; i < 256; i++){
-    mainDiv.appendChild(document.createElement('div'))
+// create a 16 x 16 grid and add event listener
+for(i = 0; i < 257; i++){
+    const squareDiv = document.createElement('div');
+
+    squareDiv.addEventListener('mouseover', changeColor);
+    function changeColor(e){
+        squareDiv.style.backgroundColor = 'black';
+    }
+
+    mainDiv.appendChild(squareDiv);
 }
 
 console.log(mainDiv);
